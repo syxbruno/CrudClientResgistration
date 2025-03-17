934368bbcbbea21294c4515cf122315c194734c5
syxbruno <syxbrunoo@gmail.com>
2025-03-17 14:35:44 -0300

    MySQL database has been replaced by PostgreSQL 🗃️
    the MySQL database has been replaced by the PostgreSQL database

--- src/main/java/com/syxbruno/CrudClientRegistration/domain/User.java
+++ src/main/java/com/syxbruno/CrudClientRegistration/domain/User.java
@@ -18,7 +18,7 @@ import java.util.List;
 @Table(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
-public class    User implements UserDetails {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
